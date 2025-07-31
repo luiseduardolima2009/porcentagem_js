92a41141ed05ca344ea483038ea3e123c6ef7a65
luiseduardolima2009 <luis.moura.lima@escola.pr.gov.br>
2025-07-31 12:16:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ function multiplicar(){
     let primeiro_numero = parseInt(document.getElementById('primeiro_numero').value);
     let segundo_numero = parseInt(document.getElementById('segundo_numero').value);
     let terceiro_numero = parseInt(document.getElementById('terceiro_numero').value);
+    segundo_numerop = segundo_numero / 100;
+    primeiro_numerop = primeiro_numero * segundo_numerop * terceiro_numero ;
 
-    let segundo_numerop = segundo_numero / 100;
-    let primeiro_numerop = primeiro_numero * segundo_numerop * terceiro_numero ;
+    document.getElementById('resultado').textContent = 'resultado: ' + segundo_numero;
 
-    document.getElementById('resultado').textContent = 'resultado: ' + primeiro_numerop;
 
+    montante = primeiro_numero + primeiro_numerop;
+
+    document.getElementById('montante').textContent = 'montante: ' + montante;
+                    
 }
